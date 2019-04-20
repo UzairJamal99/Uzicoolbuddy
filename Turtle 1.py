94042ea44710a0af3aa33f3c240b42676083a30d
UzairJamal99 <49692426+UzairJamal99@users.noreply.github.com>
2019-04-21 02:25:18 +0500

    initial commit

---
+++ Turtle 1.py
@@ -0,0 +1,27 @@
+import turtle
+L = turtle.Turtle()
+
+L.color("red")
+L.forward(100)
+L.left(90)
+L.forward(100)
+L.left(90)
+L.forward(100)
+L.left(90)
+L.forward(100)
+
+L.penup()
+L.forward(150)
+L.pendown()
+
+L.forward(100)
+L.left(90)
+L.forward(100)
+L.left(90)
+L.forward(100)
+L.left(90)
+L.forward(100)
+
+
+turtle.done()
+
